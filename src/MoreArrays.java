c5bdf8ac28e1eeb89c85c87fd419df71b7cba824
Fahima Begum <haffahima@gmail.com>
2022-10-01 23:45:46 +0100

    removed coverage

--- src/MoreArrays.java
+++ src/MoreArrays.java
@@ -7,6 +7,8 @@ public class MoreArrays {
 	int[] multiplesOf3 = new int [10];
 	
 	for (int i = 1; i <= multiplesOf3.length; i++) { //started at i=1 because going to assign each element as i*3 
+		multiplesOf3[i - 1] = i * 3; // i - 1 is element 0. So element 0 is i*3
+		System.out.println("number: " + multiplesOf3[i - 1]);
 	} 
 	}
 
