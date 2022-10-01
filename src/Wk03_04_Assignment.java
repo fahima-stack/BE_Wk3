26017e79633fac8f3da28c73b0f4ac8dd82bb6e8
Fahima Begum <haffahima@gmail.com>
2022-10-01 22:30:25 +0100

    Wk-03_04 2nd commit

--- src/Wk03_04_Assignment.java
+++ src/Wk03_04_Assignment.java
@@ -2,10 +2,21 @@
 public class Wk03_04_Assignment {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 		int[] ages = new int[8];
 		
+		ages[0] = 3;
+		ages[1] = 9;
+		ages[2] = 23;
+		ages[3] = 64;
+		ages[4] = 2;
+		ages[5] = 8;
+		ages[6] = 28;
+		ages[7] = 93;
+		
+		System.out.println(ages[1]);
+		
+		 
 	}
 
 }
