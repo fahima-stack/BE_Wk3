1558eee9d372e8130825a677d79fa2fb67d8d542
Fahima Begum <haffahima@gmail.com>
2022-10-01 22:45:00 +0100

    More Arrays first commit

--- src/MoreArrays.java
+++ src/MoreArrays.java
@@ -4,6 +4,10 @@ public class MoreArrays {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	int[] multiplesOf3 = new int [10];
+	
+	for (int i = 1; i <= multiplesOf3.length; i++) { //started at i=1 because going to assign each element as i*3 
+	} 
 	}
 
 }
