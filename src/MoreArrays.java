53ed963b7c03570148be3e1fb62c0c0346422e3f
Fahima Begum <haffahima@gmail.com>
2022-10-02 17:07:15 +0100

    More arrays final commit

--- src/MoreArrays.java
+++ src/MoreArrays.java
@@ -8,8 +8,16 @@ public class MoreArrays { //{open class{open method{method}close method}close cl
 	
 	for (int i = 1; i <= multiplesOf3.length; i++) { //started at i=1 because going to assign each element as i*3 
 		multiplesOf3[i - 1] = i * 3; // i - 1 is element 0. So element 0 is i*3
-		System.out.println("number: " + multiplesOf3[i - 1]);
+		System.out.println("number: " + multiplesOf3[i - 1]); //prints number: 3-30 [elements 1-9]
 	} 
+	
+	int[] multiplesOf5 = new int[10]; 
+	
+		for (int i = 0; i < multiplesOf5.length; i++) {
+		multiplesOf5[i] = (i + 1) * 5; //element [0] = (0+1)*5
+		System.out.println(multiplesOf5[i]); //prints 5-50 [elements 0-9]
+		}
+	
 	}
 
 }
