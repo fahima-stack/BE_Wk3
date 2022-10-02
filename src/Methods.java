67febc470072ca0046b967a820f9bf63dfc59ac4
Fahima Begum <haffahima@gmail.com>
2022-10-02 19:43:51 +0100

    Methods final commit

---
+++ src/Methods.java
@@ -0,0 +1,25 @@
+
+public class Methods {
+
+	public static void main(String[] args) { //main method
+		// creating method
+		
+		String firstName = "Bob";
+		String lastName = "Ross";
+		String fullName	= createFullName(firstName, lastName); 
+		String fullName2 = createFullName("Sam", lastName);		
+		
+		System.out.println(fullName); //prints Bob Ross
+		System.out.println(fullName2); //Prints Sam Ross
+
+	}
+
+	public static String createFullName(String x, String y) { //new method outside main and in class
+		String fullName = x + " " + y;
+		return fullName; 
+		
+	}
+} 
+
+
+
